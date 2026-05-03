98a2758719dae60d6758683d65375d4791b92a78
Ruchitk620 <157483500+Ruchitk620@users.noreply.github.com>
2026-05-03 17:47:19 +0530

    Refactor main.py for list processing and calculations
    Refactor code to filter and process lists, including calculations for even numbers, prime numbers, and Fibonacci series.

--- main.py
+++ main.py
@@ -1,205 +1,68 @@
 
-# --------- question 1 ----------------
+# Q1 - filter out 18+ people from dictionary 
+people = [
+    {"name": "Akhil", "age": 17},
+    {"name": "Ravi", "age": 20},
+    {"name": "Meena", "age": 22}
+]   # created a dictionary
 
-list = [10, 501, 22, 37, 100, 999, 87, 351]
-even=[]
-odd=[]
-
-for i in list:  #Loop for each number in lsit
-    if i % 2 == 0:    #check if even
-        even.append(i)
-    else:
-        odd.append(i)
-
-print("Even numers:",even)  # print the result
-print("Odd nummbers:",odd)
-
-#-------------- Question 2 ------------------
-prime = []
-
-for j in list: #Loop for each number in lsit
-    if j > 1:
-        is_prime = True
-
-        for k in range(2,j):
-            if j % k == 0:  # Check divisibility
-                is_prime= False
-                break
-        if is_prime:  # If still prime, add to list
-            prime.append(j)
-
-print("Prime numbers are:",prime)
-print("Count of prime numbers:",len(prime))
+print(list(filter(lambda x: x["age"] >= 18,people))) # prints the 18+ people 
 
 
-#------------- Question 3 -----------------
-happy = []
+#Q2 - calculate product of the list using reduce and lambda
+from functools import reduce
 
-for i in list:  #Loop for each number in lsit
-    n = i
-    seen = []
+numbers = [1,2,3,4]
 
-    while n != 1 and n not in seen:  # Process to check happy number
-        seen.append(n)
+print(reduce(lambda x,y : x * y , numbers))  # calculate 2 numbers from the list every time
 
-        sum_sq = 0
-        while n > 0:
-            digit = n % 10
-            sum_sq = sum_sq + digit * digit 
-            n = n // 10
+#Q3 - filter the even numbers and square the even nummbers
 
-        n = sum_sq
-
-    if n == 1:
-        happy.append(i)
-
-print("Happy numbers",happy)
-print("Count of happy numbers:",len(happy))
+numbers = [1,2,3,4,5,6]
+even  = filter(lambda x: x % 2 == 0,numbers)  # checks for the even numbers
+squares = list(map(lambda y: y * y , even))  # maps the squared numbers to the list
+print("the sqyare numbers of the even in list are",squares)
 
 
-# -------------question 4 -------------
-num = int(input("Enter the number")) # Take input from user
+#Q4 - to check the string is digit 
+# Lambda function
+is_number = lambda x: x.isdigit()  #isdigit will check the given string
 
-# Store original number
-real_num = num
+# Test cases
+print(is_number("123"))  
+print(is_number("abc"))   
+print(is_number("12a"))  
 
-last_digit = num % 10  # last digit
+#Q5 - to extract the date year from the data
+from datetime import datetime
 
-# Get first digit
-while num >= 10:
-    num = num // 10
+# Create datetime object
+dt = datetime(2025, 1, 10)
 
-first_digit = num
+# Lambda function to extract values
+get_date_parts = lambda x: (x.year, x.month, x.day)
 
-total = first_digit + last_digit
-
-print("First digit:", first_digit)
-print("Last digit:", last_digit)
-print("Sum of first and last digit:",total)
-
-
-
-# -------------- Question 5 ------------
-# Target amount
-amount = 10
-
-# Loop for each coin
-for one in range(0, 11):        # 1 coin
-    for two in range(0, 6):     # 2 coin
-        for five in range(0, 3): # 5 coin
-            for ten in range(0, 2): # 10 coin
-
-                total = (1 * one) + (2 * two) + (5 * five) + (10 * ten)
-
-                if total == amount:
-                    print("1₹:", one, "2₹:", two, "5₹:", five, "10₹:", ten)
-
-
-
-
-# -------------- Question 6 -------------
-# Given lists
-list1 = [10, 20, 30, 40]
-list2 = [30, 40, 50, 60]
-list3 = [40, 30, 70, 80]
-
-duplicates = []
-
-# Loop through first list
-for num in list1:
-    if num in list2 and num in list3:
-        duplicates.append(num)
+# Call function
+year, month, day = get_date_parts(dt)
 
 # Print result
-print("Duplicates in all 3 lists:", duplicates)
+print("Year:", year)
+print("Month:", month)
+print("Day:", day)
 
+#Q6 - Fiboncci using lambda
+# Number of terms
+n = 10
 
+# Starting values
+a = 0
+b = 1
 
+# Lambda for next number
+next_num = lambda x, y: x + y
 
-#------------- Question 7 ---------------
-# Given list
-numbers = [1, 2, 2, 3, 1, 4]
+print("Fibonacci series:")
 
-# Loop through each element
-for num in numbers:
-    count = 0
-
-    # Count occurrences
-    for i in numbers:
-        if num == i:
-            count += 1
-
-    # Check if non-repeating
-    if count == 1:
-        print("First non-repeating element:", num)
-        break
-
-
-
-# --------------- Question 8 ----------------
-# Given list
-numbers = [4, 5, 6, 1, 2, 3]
-
-# Assume first element is minimum
-minimum = numbers[0]
-
-# Loop through list
-for num in numbers:
-    if num < minimum:
-        minimum = num
-
-# Print result
-print("Minimum element:", minimum)
-
-
-#-------------- Question 9 ------------------
-# Given list and target
-numbers = [10, 20, 30, 9]
-target = 59
-
-found = False
-
-# Loop through all combinations
-for i in range(len(numbers)):
-    for j in range(i + 1, len(numbers)):
-        for k in range(j + 1, len(numbers)):
-
-            total = numbers[i] + numbers[j] + numbers[k]
-
-            if total == target:
-                print("Triplet found:", numbers[i], numbers[j], numbers[k])
-                found = True
-
-# If no triplet found
-if not found:
-    print("No triplet found")
-
-
-
-
-#-------------- Question 10 ------------------
-# Given list
-numbers = [4, 2, -3, 1, 6]
-
-found = False
-
-# Outer loop → starting point
-for i in range(len(numbers)):
-    total = 0
-
-    # Inner loop → continuous elements
-    for j in range(i, len(numbers)):
-        total = total + numbers[j]
-
-        # If sum becomes 0
-        if total == 0:
-            print("Sublist with sum 0:", numbers[i:j+1])
-            found = True
-            break
-
-    if found:
-        break
-
-# If not found
-if not found:
-    print("No sublist with sum 0")
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, next_num(a, b)
